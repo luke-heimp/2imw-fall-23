2c8c5aa3c684cbdcffdedb5a1c093cdd9c217939
luke-heimp <144061454+luke-heimp@users.noreply.github.com>
2023-10-19 13:14:00 -0400

    e6

--- E6/script.js
+++ E6/script.js
@@ -1,1 +1,19 @@
+// code from https://api.jquery.com/animate/ and https://api.jquery.com/position/
+//Jquery methods: Effects = .animate, Events = .click & .dblclick, Manipulation = .position
+
 console.log("Hello");
+
+$("#right").on("click", function () {
+  $("#block").animate({ left: "+=50px" }, "slow");
+});
+
+$("#left").on("click", function () {
+  $("#block").animate({ left: "-=50px" }, "slow");
+});
+
+$(document).on("keydown", function (event) {
+  if (event.which === 32) {
+    var x = $("#block").position();
+    alert("Top: " + x.top + " Left: " + x.left);
+  }
+});
