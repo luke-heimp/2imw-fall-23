1bb6cfc88385cdc4dfd6fc0242352812b167317e
luke-heimp <144061454+luke-heimp@users.noreply.github.com>
2023-11-08 22:15:01 -0500

    new stuff

--- E8/script.js
+++ E8/script.js
@@ -1,10 +1,18 @@
-$.getJSON(
-  "https://api.openweathermap.org/data/2.5/weather?q=Toronto,uk&APPID=4bef36e95f94e06a14ba13291dd91f94",
-  function (data) {
-    var temp = Math.floor(data.main.temp);
-    var weather = data.weather[0].main;
+// E8, Luke Heimpel
+//tutorial: https://www.youtube.com/watch?v=InoAIgBZIEA
 
-    $(".temp").append(temp);
-    $("weather").append(weather);
+$.getJSON(
+  "https://api.openweathermap.org/data/2.5/weather?lat=43.46&lon=-80.52&units=metric&appid=4bef36e95f94e06a14ba13291dd91f94&units",
+  function (data) {
+    console.log(data);
+    //variables
+    var temp = data.main.temp;
+    var description = data.weather[0].description;
+    var icon =
+      "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+    // talk to html
+    $(".temp").append(temp + "°");
+    $(".weather1").append(description);
+    $(".icon").attr("src", icon);
   }
 );
