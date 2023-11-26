ed202afb1f66b59dfaba01ccfdba986f2efd2c83
luke-heimp <144061454+luke-heimp@users.noreply.github.com>
2023-11-25 19:05:29 -0500

    nov 25

---
+++ R5/script.js
@@ -0,0 +1,52 @@
+$(function () {
+  $("#bottom").draggable();
+});
+
+$(function () {
+  $("#middle").draggable();
+});
+
+$(function () {
+  $("#top").draggable();
+});
+
+$(function () {
+  $("#stick1").draggable();
+});
+
+$(function () {
+  $("#stick2").draggable();
+});
+
+$(function () {
+  $("#nose").draggable();
+});
+
+$(function () {
+  $("#coal1").draggable();
+});
+
+$(function () {
+  $("#coal2").draggable();
+});
+
+$(function () {
+  $("#coal3").draggable();
+});
+
+$(function () {
+  $("#coal4").draggable();
+});
+$(function () {
+  $("#coal5").draggable();
+});
+$(function () {
+  $("#coal6").draggable();
+});
+
+$(function () {
+  $("#hat").draggable();
+});
+$(function () {
+  $("#smile").draggable();
+});
